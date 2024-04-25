94bce8f670cf95a511dc46b91ab48c1f4943578f
gilberto19 <gilberto19>
2024-04-24 21:45:57 -0300

    feat: implement api documentation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -8,11 +8,14 @@ import {
   Param,
   Request,
   Put,
+  Body,
+  Delete,
 } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 import { RouteEnum } from 'src/shareds/enums/routes.enum';
 import { CreateUserInput, UpdateUserInput, UserOutput } from './dtos';
 import { UserService } from './user.service';
+import { GetUserOutput } from './dtos/user-pagination';
 
 @Controller(RouteEnum.User)
 export class UserController {
@@ -22,8 +25,9 @@ export class UserController {
   @ApiOkResponse({ description: 'Successfully', type: UserOutput })
   @Post()
   @HttpCode(HttpStatus.OK)
-  create(@Req() req: Request): Promise<UserOutput> {
-    return this.userService.create(<CreateUserInput>req['user']);
+  create(@Body() data: CreateUserInput): Promise<UserOutput> {
+    console.log(data);
+    return this.userService.create(data);
   }
 
   @ApiOperation({ summary: 'Create user endpoint' })
@@ -34,11 +38,26 @@ export class UserController {
     return this.userService.update(id, <UpdateUserInput>req['user']);
   }
 
-  @ApiOperation({ summary: 'list user endpoint' })
+  @ApiOperation({ summary: 'get user endpoint' })
   @ApiOkResponse({ description: 'Successfully', type: UserOutput })
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   getUser(@Param('id') id: string): Promise<UserOutput> {
     return this.userService.getOne(id);
+  }
+
+  @ApiOperation({ summary: 'list user endpoint' })
+  @ApiOkResponse({ description: 'Successfully', type: GetUserOutput })
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  listAll(): Promise<GetUserOutput> {
+    return this.userService.getAll();
+  }
+  @ApiOperation({ summary: 'Delete user endpoint' })
+  @ApiOkResponse({ description: 'Successfully', type: UserOutput })
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  deleteUser(@Param('id') id: string): Promise<void> {
+    return this.userService.delete(id);
   }
 }
